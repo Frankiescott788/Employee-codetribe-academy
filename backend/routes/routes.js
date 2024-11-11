984bc58968d08a292c9da991ed78885a4caaba7d
Frankiescott788 <130049924+Frankiescott788@users.noreply.github.com>
2024-11-11 10:25:20 +0200

    Update routes.js

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,17 +1,17 @@
+// routes/routes.js
 const { Router } = require('express');
-const {getEmployees, addEmployees, getEmployee, deleteEmployee}  = require('../controllers/controllers');
+const { getEmployees, addEmployees, getEmployee, deleteEmployee } = require('../controllers/controllers');
 const multer = require('multer');
 const { updateDoc } = require('firebase/firestore');
 
 const routes = Router();
-
 const storage = multer.memoryStorage();
-const upload = multer({storage});
+const upload = multer({ storage });
 
-routes.get('/api/employees', getEmployees);
-routes.get('/api/employees/:id', getEmployee);
-routes.post('/api/employees', upload.single('photo') , addEmployees);
-routes.patch('/api/employees/:id', upload.single('photo'), updateDoc);
-routes.delete('/api/employees/:id', deleteEmployee);
+routes.get('/employees', getEmployees);
+routes.get('/employees/:id', getEmployee);
+routes.post('/employees', upload.single('photo'), addEmployees);
+routes.patch('/employees/:id', upload.single('photo'), updateDoc);
+routes.delete('/employees/:id', deleteEmployee);
 
 module.exports = routes;
