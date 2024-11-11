0d9084bb2ba48eab0d3f5b848e0b8d96ddc708aa
Frankiescott788 <130049924+Frankiescott788@users.noreply.github.com>
2024-11-11 10:25:50 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,19 @@
+// index.js
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes/routes");
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({}));
 
-// Use the PORT environment variable provided by Render, defaulting to 8080 if not set
+// Set port
 const PORT = process.env.PORT || 8080;
 
+// Use routes
+app.use('/api', routes);
+
 app.listen(PORT, () => {
-  app.use(routes);
-  console.log(`Server connected to database and running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
-
